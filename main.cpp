c5fccd36fc045a4ba2e27b6618cb026f67d9570c
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-10-01 22:15:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <iomanip>
 using namespace std;
 bool notNumber(char a){
     char letras[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -18,7 +19,6 @@ int finalIndex(char ind){
     for( int i  = 0; i < 26; i++)
         if (letras[i] == ind)
             return i;
-    return 0;
 }
 
 int final(string a){
@@ -26,47 +26,55 @@ int final(string a){
         return finalIndex(a[0]);
     }
     else if (a.size() == 2){
-        return 26*(finalIndex(a[0])+1) + finalIndex(a[1])+1;
+        return 26*(finalIndex(a[0])+1) + finalIndex(a[1]);
     }
     else{
-        return 1;
+        return pow(26,2)*(finalIndex(a[0])+1) + 26*(finalIndex(a[0])+1) + finalIndex(a[1]) ;
     }
 }
-string getCelvalue(string &celda, string** celdas){
-    if(isSolved(celda)){
+string getCelvalue(string &celda, string** celdas) {
+    if (isSolved(celda)) {
         return celda;
-    }
-    else{
+    } else {
         vector<string> subcels;
         string f = "";
-        for(int i = 1; i < celda.size();i++){
-            if(celda[i] != '+')
+        for (int i = 1; i < celda.size(); i++) {
+            if (celda[i] != '+')
                 f = f + celda[i];
-            else{
+            else {
                 subcels.push_back(f);
                 f = "";
             }
-            if(i == celda.size()-1)
+            if (i == celda.size() - 1)
                 subcels.push_back(f);
         }
         //cambiar parte
-        for(int i  = 0; i  < subcels.size(); i++){
-            int column = finalIndex(subcels[i][0]);
-            char a = subcels[i][1];
-            int ia = a - '0';
+        for (int i = 0; i < subcels.size(); i++) {
+            string index;
+            string column;
+            string a = subcels[i];
+            for (int j = 0; j < a.size(); j++) {
+                if (notNumber(a[j])) {
+                    column = column + a[j];
+                } else {
+                    index = index + a[j];
+                }
+            }
+            int fin_col = final(column);
             int row;
-            row = ia-1;
-            subcels[i] = getCelvalue(celdas[row][column], celdas);
+            row = stoi(index);
+            subcels[i] = getCelvalue(celdas[row-1][fin_col], celdas);
+            }
+            //aca
+            int finalRes = 0;
+            for (int i = 0; i < subcels.size(); i++) {
+                finalRes = finalRes + stoi(subcels[i]);
+            }
+            string res = to_string(finalRes);
+            return res;
         }
-        //aca
-        int finalRes = 0;
-        for(int i = 0; i < subcels.size(); i++){
-            finalRes = finalRes + stoi(subcels[i]);
-        }
-        string res = to_string(finalRes);
-        return res;
     }
-}
+
 
 void solveMat(string** celdas,int filas, int columnas){
     for(int i = 0; i < filas ; i ++){
@@ -78,40 +86,28 @@ void solveMat(string** celdas,int filas, int columnas){
 void print(string** celdas,int filas, int columnas){
     for(int i = 0; i < filas ; i ++){
         for(int j = 0; j < columnas; j++){
-            cout << celdas[i][j] << " ";
+            cout   << celdas[i][j]  << " ";
         }
         cout << endl;
     }
 }
 int main() {
-    int filas,columnas;
-    cin >> columnas >> filas;
-    string ** celdas;
-    celdas = new string*[filas];
-    for(int i  = 0; i < filas; i++){
-        celdas[i] = new string[columnas];
-    }
-    for(int i  = 0 ; i < filas; i++){
-        for(int j = 0; j < columnas; j++){
-            cin >> celdas[i][j];
+    int numExcels;
+    cin >> numExcels;
+    for(int p = 0 ; p < numExcels; p++) {
+        int filas, columnas;
+        cin >> columnas >> filas;
+        string **celdas;
+        celdas = new string *[filas];
+        for (int i = 0; i < filas; i++) {
+            celdas[i] = new string[columnas];
         }
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                cin >> celdas[i][j];
+            }
+        }
+        solveMat(celdas, filas, columnas);
+        print(celdas, filas, columnas);
     }
-    solveMat(celdas,filas,columnas);
-    print(celdas,filas,columnas);
-
-
-
 }
-
-//string a = "AA22";
-//string index ;
-//string column;
-//for (int i = 0; i < a.size(); i++) {
-//if(notNumber(a[i])){
-//column = column + a[i];
-//}
-//else{
-//index = index + a[i];
-//}
-//}
-//cout << column  << " " << index;
